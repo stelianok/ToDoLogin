476075993c46db5c53d90d9e364f9f965acc0e71
Kau <stelianok@gmail.com>
2020-06-03 10:56:28 -0300

    Header component added to Login page

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <>
       <StatusBar backgroundColor="#383FDB" />
-      <Welcome />
+      <Login />
     </>
   );
 }
--- Mobile/src/screens/Login/index.js
+++ Mobile/src/screens/Login/index.js
@@ -4,9 +4,13 @@ import {View, Text, Button, Input} from 'react-native';
 import styles from './styles';
 import Header from '../../components/Header';
 export default function Login() {
-  return(
-    <View>
-
+  return (
+    <View style={styles.container}>
+      <Header
+        title="Sign Up"
+        text="Please Registration with email and
+              sign up to continue using our app"
+      />
     </View>
-    );
+  );
 }
