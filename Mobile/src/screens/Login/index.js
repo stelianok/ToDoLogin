c646b3fd6b9141793b72da08c822093e110bd13e
Kau <stelianok@gmail.com>
2020-06-03 08:56:36 -0300

    Header Ready

--- Mobile/src/screens/Login/index.js
+++ Mobile/src/screens/Login/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 import styles from './styles';
+
+//import Logo from '../../img';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text> Login Page </Text>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Welcome </Text>
+        <Text style={styles.headerText}>Please Login or sign up to continue using our app </Text>
+      </View>
+      
     </View>
   );
 }
--- Mobile/src/screens/Login/styles.js
+++ Mobile/src/screens/Login/styles.js
@@ -2,8 +2,27 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'indigo',
+    backgroundColor: '#FCFDFE',
     flex: 1,
   },
+  header: {
+    marginHorizontal: 25,
+    marginVertical: 50,
+
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  headerTitle: {
+    color: '#383FDB',
+    fontWeight: 'bold',
+    fontSize: 32,
+
+    paddingBottom: 20,
+  },
+  headerText: {
+    color: '#604A65',
+    fontSize: 18,
+  },
+
 });
 export default styles;
