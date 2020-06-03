b52fb9301568c3323ce4de2d44747e958c2d6949
Kau <stelianok@gmail.com>
2020-06-03 08:39:43 -0300

    Basic file  structure and organization created

---
+++ Mobile/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Login from './screens/Login';
+import Register from './screens/Register';
+
+export default function App() {
+  return <Login />;
+}
