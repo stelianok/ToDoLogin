3521dcf943a41fbc14e5a83b3c8c0d16ba967c3e
Kau <stelianok@gmail.com>
2020-06-04 09:21:23 -0300

    dependency react-native-vector-icons added, Login Page done

--- Mobile/src/screens/Login/index.js
+++ Mobile/src/screens/Login/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import {View, Text, Button, Input} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  useWindowDimensions,
+} from 'react-native';
 
 import styles from './styles';
 import Header from '../../components/Header';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 export default function Login() {
+  const windowsWidth = useWindowDimensions().width;
+  const [showPass, setShowPass] = useState(true);
+  const [press, setPress] = useState(false);
+
+  function ShowPassword(press) {
+    if (press) {
+      setPress(false);
+      setShowPass(true);
+    } else if (press == false) {
+      setPress(true);
+      setShowPass(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header
@@ -11,6 +33,68 @@ export default function Login() {
         text="Please Registration with email and
               sign up to continue using our app"
       />
+      <View style={styles.bodyContainer}>
+        <Text style={[styles.bodyText, {color: '#383FDB'}]}>
+          Login With Facebook
+        </Text>
+        <TouchableOpacity style={styles.facebookButton}>
+          <Text style={{color: '#FFF', fontSize: 25}}> Facebook </Text>
+        </TouchableOpacity>
+        <Text style={[styles.bodyText, {color: '#383FDB', marginTop: 30}]}>
+          Or Login with email
+        </Text>
+        <View>
+          <Icon
+            name={'ios-person'}
+            size={28}
+            color={'blue'}
+            style={styles.inputIcon}
+            underlineColorAndroid="transparent"
+          />
+          <TextInput
+            style={[styles.textInput, {width: windowsWidth - 55}]}
+            placeholder={'Username'}
+            placecholderTextColor={'gray'}
+          />
+        </View>
+        <View>
+          <Icon
+            name={'ios-lock'}
+            size={28}
+            color={'blue'}
+            style={styles.inputIcon}
+            underlineColorAndroid="transparent"
+          />
+          <TextInput
+            style={[styles.textInput, {width: windowsWidth - 55}]}
+            placeholder={'Password'}
+            secureTextEntry={showPass}
+            placecholderTextColor={'gray'}
+          />
+          <TouchableOpacity
+            style={styles.buttonEye}
+            onPress={() => {
+              ShowPassword(press);
+            }}>
+            <Icon
+              name={press === false ? 'ios-eye-off' : 'ios-eye'}
+              size={26}
+              color={'blue'}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={[styles.loginButton, {width: windowsWidth - 55}]}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <View style={styles.bottomContainer}>
+          <Text style={styles.bottomText}>Don't have an account yet?</Text>
+          <TouchableOpacity>
+            <Text style={styles.registerButtonText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- Mobile/src/screens/Login/styles.js
+++ Mobile/src/screens/Login/styles.js
@@ -4,5 +4,79 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  bodyContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    marginHorizontal: 0,
+  },
+  bodyText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+
+    marginBottom: 30,
+  },
+  facebookButton: {
+    backgroundColor: '#383FDB',
+
+    borderRadius: 15,
+    paddingHorizontal: 55,
+    paddingVertical: 15,
+  },
+  loginButton: {
+    backgroundColor: '#383FDB',
+
+    height: 60,
+    borderRadius: 15,
+
+    marginBottom: 25,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+  },
+  bottomText: {
+    fontSize: 18,
+    color: '#000',
+  },
+  registerButtonText: {
+    fontSize: 18,
+    color: '#383FDB',
+    fontWeight: 'bold',
+
+    paddingLeft: 5,
+
+    textDecorationLine: 'underline',
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 45,
+    fontSize: 16,
+    paddingLeft: 45,
+    backgroundColor: 'transparent',
+    color: 'gray',
+
+    borderColor: '#000',
+    borderWidth: 2,
+    marginHorizontal: 25,
+    marginBottom: 36,
+  },
+  inputIcon: {
+    position: 'absolute',
+    top: 10,
+    left: 37,
+  },
+  buttonEye: {
+    position: 'absolute',
+    top: 10,
+    right: 37,
+  },
 });
 export default styles;
